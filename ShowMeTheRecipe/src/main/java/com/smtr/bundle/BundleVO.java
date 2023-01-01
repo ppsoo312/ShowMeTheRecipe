9d769e820c11f68881a7563c076eef6b3f26d914
parkyounsoo <parkyounsoo@DESKTOP-LKIT406>
2023-01-02 00:42:01 +0900

    hungry

--- ShowMeTheRecipe/src/main/java/com/smtr/bundle/BundleVO.java
+++ ShowMeTheRecipe/src/main/java/com/smtr/bundle/BundleVO.java
@@ -1,9 +1,9 @@
 package com.smtr.bundle;
 
 public class BundleVO {
-	private int bundleNo; // ��ṭ�� ������
-	private String bundle; // ������
-	private int recipeNo; // ������ ������ 왜 안되
+	private int bundleNo; // 배고파
+	private String bundle; // 김치전 먹으면
+	private int recipeNo; // 살찐다
 	
 	public BundleVO(int bundleNo, String bundle, int recipeNo) {
 		this.bundleNo = bundleNo;
