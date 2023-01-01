b90f9f1114b6daae051e8093af79521bb43ed1a8
parkyounsoo <parkyounsoo@DESKTOP-LKIT406>
2023-01-02 02:32:31 +0900

    VO rename

--- ShowMeTheRecipe/src/main/java/com/smtr/cookery/CookeryVO.java
+++ ShowMeTheRecipe/src/main/java/com/smtr/cookery/CookeryVO.java
@@ -1,13 +1,13 @@
 package com.smtr.cookery;
 
-public class Cookery {
+public class CookeryVO {
 	private int cookeryNo; // 방법별 시퀀스
 	private String cookery; // 방법별 이름
 	
-	public Cookery() {
+	public CookeryVO() {
 		
 	}
-	public Cookery(int cookeryNo, String cookery) {
+	public CookeryVO(int cookeryNo, String cookery) {
 		this.cookeryNo = cookeryNo;
 		this.cookery = cookery;
 	}
